17da42ea1b5bc39d0b515438a1afae824aff1aec
Bibek-Gupta14 <biplabgupta50@gmail.com>
2026-03-10 21:20:06 +0530

    Changes in Contact

--- components/contact.tsx
+++ components/contact.tsx
@@ -97,7 +97,7 @@ export function Contact() {
                   className="flex-1 border-border hover:border-primary hover:text-secondary bg-transparent"
                 >
                   <a
-                    href="https://www.linkedin.com/in/bibek-gupta"
+                    href="https://www.linkedin.com/in/bibekgupta14/"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
